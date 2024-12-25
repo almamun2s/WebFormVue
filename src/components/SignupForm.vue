6ed27895fc563b378a593b6d4746dfb105247b6a
Abdullah Almamun <mamun2social@gmail.com>
2024-12-25 19:39:38 +0600

    feat: created signup form vue file

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,38 @@
+<template>
+  <form>
+    <label>Email:</label>
+    <input type="email" required />
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+form{
+    max-width: 420px;
+    margin: 30px auto;
+    background-color: #fff;
+    text-align: left;
+    padding: 40px;
+    border-radius: 10px;
+}
+label{
+    display: inline-block;
+    margin: 25px 0 15px;
+    font-size: 0.6em;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    font-weight: bold;
+}
+input{
+    padding: 10px 6px;
+    display: block;
+    width: 100%;
+    box-sizing: border-box;
+    border: none;
+    border-bottom: 1px solid #ddd;
+    color: #555;
+}
+</style>
